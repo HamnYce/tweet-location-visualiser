af7caad18b4424be21147988c7746c314c01cb32
HamnYce <hamadboa@gmail.com>
2025-01-18 21:30:55 +0300

    Refactor project structure: update .gitignore, remove readme, and add requirements.txt; simplify app layout with Bootstrap

--- app.py
+++ app.py
@@ -1,123 +1,14 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import Input, Output, State, html
-
-KISR_LOGO = 'assets/imgs/background.png'
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY],
-                assets_folder='assets/', use_pages=True)
 
 
-def create_nav_item(name, path, href):
-    return dbc.NavItem(dbc.NavLink(f"{name} - {path}", href=href))
-
-
-def create_links():
-    return dbc.Row(
-        class_name="ms-auto",
-        style=dict(border='1px solid yellow', height='100%'),
-        children=[
-            dbc.Col(
-                style=dict(height='100%'),
-                children=[
-                    dbc.NavItem(
-                        style=dict(height='100%'),
-                        children=[
-                            dbc.NavLink(
-                                href=f"{page['relative_path']}",
-                                style=dict(
-                                    border='1px solid red',
-                                    height='100%'
-                                ),
-                                children=f"{page['name']}",
-                            ),
-                        ]
-                    )
-                ],
-            )
-            for page in dash.page_registry.values()
-        ],
-    )
-
-
-app.layout = dbc.Container(
-    style=dict(
-        backgroundImage='url(assets/imgs/background.png)',
-        backgroundSize='100% 100%',
-        height='100%',
-        width='100%',
-    ),
-    fluid=True,
-    children=[
-        dbc.Row(
-            children=[
-                dbc.Col(
-                    children=[
-                        dbc.Container(
-                            class_name='p-2',
-                            fluid=True,
-                            children=dbc.Navbar(
-                                style=dict(borderRadius='5px', ),
-                                color='dark',
-                                sticky='top',
-                                expand='xl',
-                                children=[
-                                    dbc.Row(
-                                        children=[
-                                            dbc.Col(
-                                                html.A(
-                                                    html.Img(
-                                                        src='assets/imgs/logo.png',
-                                                        height='60px'),
-                                                    href='https://www.kisr.edu.kw/en/',
-                                                    target='_blank'
-                                                )
-                                            )
-                                        ]
-                                    ),
-                                    dbc.NavbarToggler(
-                                        id="navbar-toggler",
-                                        n_clicks=0
-                                    ),
-                                    dbc.Collapse(
-                                        id="navbar-collapse",
-                                        is_open=False,
-                                        navbar=True,
-                                        children=[
-                                            dbc.NavItem(
-                                                dbc.NavLink('Live Feed',
-                                                            href='/')
-                                            ),
-                                            dbc.NavItem(
-                                                dbc.NavLink(
-                                                    'Historical Data',
-                                                    href='/historical'
-                                                )
-                                            ),
-                                        ],
-                                    )
-                                ],
-                            ),
-                        ),
-                        dash.page_container
-                    ]
-                )
-            ]
-        )
-    ]
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
-
-@app.callback(
-    Output("navbar-collapse", "is_open"),
-    [Input("navbar-toggler", "n_clicks")],
-    [State("navbar-collapse", "is_open")],
+app.layout = dbc.Alert(
+    "Hello, Bootstrap!", className="m-5"
 )
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
 
-
-if __name__ == '__main__':
-    app.run_server(port=3001, debug=True)
+if __name__ == "__main__":
+    app.run_server()
